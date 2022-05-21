60b64f1b381faef7b287ea8f1eaaf8962ae7ea85
Humaira Rahaman <humairamitul@gmail.com>
2022-05-21 22:21:20 +0600

    clean the code

--- src/components/Decode/Decode.js
+++ src/components/Decode/Decode.js
@@ -59,107 +59,95 @@ const alphabet = [
 const Decode = () => {
   const [inputValue, setInputValue] = useState("");
   const [encodeValue, setEncodeValue] = useState("");
-  // const previousInputValue = useRef("");
 
-  // useEffect(() => {
-  //   previousInputValue.current = inputValue;
-   
-  //   const shiftNumber = 3;
-  //   const length = alphabet.length;
-  //   let ans = "";
-  //   // let ranNewPosition = ""
+  const handelDecode = (e) => {
+    e.preventDefault();
 
-  //   for (let i = 0; i < inputValue.length; i++) {
-  //     if (inputValue[i] === "." ) {
-  //       ans += " ";
-  //     } else {
-  //       let position = alphabet.indexOf(inputValue[i]);
-  //       let newPosition = (position - shiftNumber) % length;
-  //       ans += alphabet[newPosition];
-  //       // ranNewPosition = ans - 
-  //     }
-  //   }
-  //   console.log(typeof(ans));
-  //   setEncodeValue(ans);
-  // }, [inputValue]);
+    let inputString = inputValue;
+    const shiftNumber = 3;
+    const length = alphabet.length;
+    let ans = "";
+    let ranNewPosition = "";
+    let newInputString = "";
 
+    ranNewPosition = inputString.slice(1, inputString.length);
+    newInputString = ranNewPosition;
+    for (let i = 0; i < ranNewPosition.length; i++) {
+      if (ranNewPosition[i] === ".") {
+        ans += " ";
+      } else {
+        let position = alphabet.indexOf(ranNewPosition[i]);
+        let newPosition = (position - shiftNumber) % length;
 
-  const handelDecode= e=>{
-    e.preventDefault()
-      // setInputValue(e.target.value)
-        let inputString = inputValue
-        const shiftNumber = 3;
-        const length = alphabet.length;
-        let ans = "";
-        let ranNewPosition = ""
-        let newInputString = ""
-          // let x = Math.floor((Math.random() * 9)+1);
-          ranNewPosition = inputString.slice(1, inputString.length);
-          newInputString = ranNewPosition;
-        for (let i = 0; i < ranNewPosition.length; i++) {
-         
-          if (ranNewPosition[i] === ".") {
-            ans += " ";
-          } else {
-            
-             
-            let position = alphabet.indexOf(ranNewPosition[i]);
-            let newPosition = (position - shiftNumber) % length;
-            
-            ans += alphabet[newPosition];
-            
-           
-          }
-        }
-    
-    console.log(inputString)
-     
+        ans += alphabet[newPosition];
+      }
+    }
+
+    console.log(inputString);
+
     console.log(newInputString);
     setEncodeValue(ans);
-    
-  }
-
+  };
 
   return (
     <div className="hero min-h-screen bg-indigo-50 shadow-lg shadow-indigo-300">
-      <div data-aos="zoom-out" data-aos-delay="300" data-aos-duration="1000" className="card flex-shrink-0 w-full max-w-xl shadow-2xl bg-base-100">
+      <div
+        data-aos="zoom-out"
+        data-aos-delay="300"
+        data-aos-duration="1000"
+        className="card flex-shrink-0 w-full max-w-xl shadow-2xl bg-base-100"
+      >
         <div className="card-body">
-        <form data-aos="flip-down" data-aos-delay="600" data-aos-duration="1000" onClick={handelDecode} >
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text text-indigo-600 font-semibold">Input Text</span>
-            </label>
-            <input
-             value={inputValue || ''}
-             onChange={(e) => setInputValue(e.target.value)}
-              type="text"
-              placeholder="Type your encoded Text..."
-              className="input input-bordered shadow-lg shadow-gray-200"
-            />
-          </div>
-          <div className="form-control mt-6">
-            <button className="btn btn-primary bg-gradient-to-r from-blue-500 via-indigo-400 to-cyan-400 border-0 hover:from-cyan-400 hover:via-indigo-400 hover:to-blue-500">Decode Message</button>
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text text-indigo-600 font-semibold">Decoded Message</span>
-            </label>
-            
-            {/* <input
-              type="text"
-              placeholder="Decoded
-              Message"
-              className="input input-bordered"
-            /> */}
-            <label className="label">
-              <Link  to="/encode" className="label-text-alt text-cyan-500 text-sm  link link-hover">
-                Encode Text?
-              </Link>
-            </label>
+          <form
+            data-aos="flip-down"
+            data-aos-delay="600"
+            data-aos-duration="1000"
+            onClick={handelDecode}
+          >
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-indigo-600 font-semibold">
+                  Input Text
+                </span>
+              </label>
+              <input
+                value={inputValue || ""}
+                onChange={(e) => setInputValue(e.target.value)}
+                type="text"
+                placeholder="Type your encoded Text..."
+                className="input input-bordered shadow-lg shadow-gray-200"
+              />
             </div>
-          
-        </form>
-        <p data-aos="fade-down" data-aos-delay="900" data-aos-duration="1000" className=" shadow-lg shadow-gray-200  py-5 rounded px-3 border-2">{encodeValue}</p>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary bg-gradient-to-r from-blue-500 via-indigo-400 to-cyan-400 border-0 hover:from-cyan-400 hover:via-indigo-400 hover:to-blue-500">
+                Decode Message
+              </button>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-indigo-600 font-semibold">
+                  Decoded Message
+                </span>
+              </label>
+
+              <label className="label">
+                <Link
+                  to="/encode"
+                  className="label-text-alt text-cyan-500 text-sm  link link-hover"
+                >
+                  Encode Text?
+                </Link>
+              </label>
+            </div>
+          </form>
+          <p
+            data-aos="fade-down"
+            data-aos-delay="900"
+            data-aos-duration="1000"
+            className=" shadow-lg shadow-gray-200  py-5 rounded px-3 border-2"
+          >
+            {encodeValue}
+          </p>
         </div>
       </div>
     </div>
--- src/components/Encode/Encode.js
+++ src/components/Encode/Encode.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
 
-
 const alphabet = [
   "A",
   "B",
@@ -60,72 +59,91 @@ const Encode = () => {
   const [inputValue, setInputValue] = useState("");
   const [encodeValue, setEncodeValue] = useState("");
 
+  const handelEncode = (e) => {
+    e.preventDefault();
 
-  const handelEncode= e=>{
-    e.preventDefault()
-     
-        let inputString = inputValue
-        const shiftNumber = 3;
-        const length = alphabet.length;
-        let ans = "";
-        let ranNewPosition = ""
-          let x = Math.floor((Math.random() * 9)+1);
-    
-        for (let i = 0; i < inputString.length; i++) {
-          if (inputString[i] === " ") {
-            ans += ".";
-          } else {
-            let position = alphabet.indexOf(inputString[i]);
-            let newPosition = (position + shiftNumber) % length;
-            
-            ans += alphabet[newPosition];
-            ranNewPosition = x + ans;
-           
-          }
-        }
-    
-   
+    let inputString = inputValue;
+    const shiftNumber = 3;
+    const length = alphabet.length;
+    let ans = "";
+    let ranNewPosition = "";
+    let x = Math.floor(Math.random() * 9 + 1);
+
+    for (let i = 0; i < inputString.length; i++) {
+      if (inputString[i] === " ") {
+        ans += ".";
+      } else {
+        let position = alphabet.indexOf(inputString[i]);
+        let newPosition = (position + shiftNumber) % length;
+
+        ans += alphabet[newPosition];
+        ranNewPosition = x + ans;
+      }
+    }
+
     setEncodeValue(ranNewPosition);
-    
-  }
+  };
 
   return (
     <div className="hero min-h-screen shadow-lg shadow-indigo-300">
-      <div data-aos="zoom-in" data-aos-delay="300" data-aos-duration="1000" className="card flex-shrink-0 w-full max-w-xl  shadow-2xl bg-base-100">
+      <div
+        data-aos="zoom-in"
+        data-aos-delay="300"
+        data-aos-duration="1000"
+        className="card flex-shrink-0 w-full max-w-xl  shadow-2xl bg-base-100"
+      >
         <div className="card-body">
-        <form data-aos="flip-up" data-aos-delay="600" data-aos-duration="1000" onClick={handelEncode} >
-          <div   className="form-control">
-            <label className="label">
-              <span className="label-text text-indigo-600 font-semibold">Input Text</span>
-            </label>
-            <input
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-              type="text"
-              placeholder="Type your message..."
-              className="input input-bordered shadow-lg shadow-gray-200"
-            />
-            {/* <h2>Current Value: </h2>
-            <h2>Previous Value: {previousInputValue.current}</h2> */}
-          </div>
+          <form
+            data-aos="flip-up"
+            data-aos-delay="600"
+            data-aos-duration="1000"
+            onClick={handelEncode}
+          >
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-indigo-600 font-semibold">
+                  Input Text
+                </span>
+              </label>
+              <input
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                type="text"
+                placeholder="Type your message..."
+                className="input input-bordered shadow-lg shadow-gray-200"
+              />
+            </div>
 
-          <div className="form-control mt-6">
-            <button  className="btn btn-primary bg-gradient-to-r from-blue-500 via-indigo-400 to-cyan-400 border-0 hover:from-cyan-400 hover:via-indigo-400 hover:to-blue-500">Encode Message</button>
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text text-indigo-600 font-semibold ">Encoded Text</span>
-            </label>
-            
-           
-            <label className="label">
-              <Link to="/decode" className="label-text-alt text-cyan-500 text-sm  link link-hover">
-                Decode Message?
-              </Link>
-            </label>
-          </div>
-        </form>
-        <p data-aos="fade-up" data-aos-delay="900" data-aos-duration="1000" className=" shadow-lg shadow-gray-200 read-only  py-5 rounded px-3 border-2">{encodeValue}</p>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary bg-gradient-to-r from-blue-500 via-indigo-400 to-cyan-400 border-0 hover:from-cyan-400 hover:via-indigo-400 hover:to-blue-500">
+                Encode Message
+              </button>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-indigo-600 font-semibold ">
+                  Encoded Text
+                </span>
+              </label>
+
+              <label className="label">
+                <Link
+                  to="/decode"
+                  className="label-text-alt text-cyan-500 text-sm  link link-hover"
+                >
+                  Decode Message?
+                </Link>
+              </label>
+            </div>
+          </form>
+          <p
+            data-aos="fade-up"
+            data-aos-delay="900"
+            data-aos-duration="1000"
+            className=" shadow-lg shadow-gray-200 read-only  py-5 rounded px-3 border-2"
+          >
+            {encodeValue}
+          </p>
         </div>
       </div>
     </div>
